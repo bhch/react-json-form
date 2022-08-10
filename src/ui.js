cc387a1612985ba1cc75adc037d916cdcfeb5e91
Bharat Chauhan <tell.bhch@gmail.com>
2022-08-10 17:43:14 +0530

    Make a copy of sub schema for object keys to avoid mutation

--- src/ui.js
+++ src/ui.js
@@ -321,7 +321,7 @@ export function getObjectFormRow(args) {
         let key = keys[i];
         let value = data[key];
         let childName = name + '-' + key;
-        let schemaValue = schema_keys[key];
+        let schemaValue = {...schema_keys[key]};
 
         if (typeof schemaValue === 'undefined') {
             // for keys added through additionalProperties
