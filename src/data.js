a759b23dff7c1f55a8e2d65d75d9c95046ba3fd3
Bharat Chauhan <tell.bhch@gmail.com>
2022-08-14 12:58:34 +0530

    Fix #39: Respect minItems while creating and syncing arrays

--- src/data.js
+++ src/data.js
@@ -35,12 +35,15 @@ export function getBlankObject(schema, getRef) {
 
 
 export function getBlankArray(schema, getRef) {
-    if (schema.default)
+    let minItems = schema.minItems || schema.min_items || 0;
+
+    if (schema.default && schema.default.length >= minItems)
         return schema.default;
 
     let items = [];
 
-    let minItems = schema.minItems || schema.min_items || 0;
+    if (schema.default)
+        items = [...schema.default];
 
     if (minItems === 0)
         return items;
@@ -59,23 +62,29 @@ export function getBlankArray(schema, getRef) {
         type = 'object';
 
     if (type === 'array') {
-        items.push(getBlankArray(schema.items, getRef))
+        while (items.length < minItems)
+            items.push(getBlankArray(schema.items, getRef));
         return items;
-    }
-    else if (type === 'object') {
-        items.push(getBlankObject(schema.items, getRef));
+    } else if (type === 'object') {
+        while (items.length < minItems)
+            items.push(getBlankObject(schema.items, getRef));
         return items;
     }
 
     if (schema.items.widget === 'multiselect')
         return items;
 
-    if (type === 'boolean')
-        items.push(schema.items.default === false ? false : (schema.items.default || null));
-    else if (type === 'integer' || type === 'number')
-        items.push(schema.items.default === 0 ? 0 : (schema.items.default || null));
-    else // string, etc.
-        items.push(schema.items.default || '');
+    if (type === 'boolean') {
+        while (items.length < minItems)
+            items.push(schema.items.default === false ? false : (schema.items.default || null));
+    } else if (type === 'integer' || type === 'number') {
+        while (items.length < minItems)
+            items.push(schema.items.default === 0 ? 0 : (schema.items.default || null));
+    } else {
+        // string, etc.
+        while (items.length < minItems)
+            items.push(schema.items.default || '');
+    }
 
     return items;
 }
@@ -116,23 +125,38 @@ function getSyncedArray(data, schema, getRef) {
     }
 
     let type = schema.items.type;
+    let minItems = schema.minItems || schema.min_items || 0;
     
     if (type === 'list')
         type = 'array';
     else if (type === 'dict')
         type = 'object';
 
+    const filler = '__JSONRORM_FILLER__'; // filler for minItems
+
+    while (data.length < minItems)
+        data.push(filler);
+
     for (let i = 0; i < data.length; i++) {
         let item = data[i];
 
         if (type === 'array') {
+            if (item === filler)
+                item = [];
             newData[i] = getSyncedArray(item, schema.items, getRef);
         } else if (type === 'object') {
+            if (item === filler)
+                item = {};
             newData[i] = getSyncedObject(item, schema.items, getRef);
-        }
-        else {
-            if ((type === 'integer' || type === 'number') && item === '')
-                newData[i] = schema.items.default === 0 ? 0 : (schema.items.default || null);
+        } else {
+            if (item === filler) {
+                if (type === 'integer' || type === 'number')
+                    newData[i] = schema.items.default === 0 ? 0 : (schema.items.default || null);
+                else if (type === 'boolean')
+                    newData[i] = schema.items.default === false ? false : (schema.items.default || null);
+                else
+                    newData[i] = schema.items.default || '';
+            }
         }
     }
 
@@ -180,10 +204,14 @@ function getSyncedObject(data, schema, getRef) {
             else if (type === 'object')
                 newData[key] = getSyncedObject(data[key], schemaValue, getRef);
             else {
-                if ((type === 'integer' || type === 'number') && data[key] === '')
-                    newData[key] = schemaValue.default === 0 ? 0 : (schemaValue.default || null);
-                else
+                if (data[key] === '') {
+                    if (type === 'integer' || type === 'number')
+                        newData[key] = schemaValue.default === 0 ? 0 : (schemaValue.default || null);
+                    else if (type === 'boolean')
+                        newData[key] = schemaValue.default === false ? false : (schemaValue.default || null);
+                } else {
                     newData[key] = data[key];
+                }
             }
         }
         
