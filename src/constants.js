ef8339b991a0d8d1b8033969cbbbb5f23bcdf891
Bharat Chauhan <tell.bhch@gmail.com>
2022-11-17 08:54:51 +0530

    Add constants

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+/* Symbol for joining coordinates.
+ * Earlier, a hyphen (-) was used. But that caused problems when
+ * object keys had hyphen in them. So, we're switching to a less
+ * commonly used symbol.
+*/
+export const JOIN_SYMBOL = '§';
+
+/* HTML field name prefix */
+export const FIELD_NAME_PREFIX = 'rjf';
+
+/* Filler item for arrays to make them at least minItems long */
+export const FILLER = '__RJF_FILLER__';
